bd0d15a3c0ced49a0583dac36eab11553b6d1d9c
Astemir Afashagov <astemirafashagov@MacBook-Pro-van-Astemir.local>
2026-02-05 22:31:31 +0300

    feat: brands/models database — 50+ brands for Russian market

---
+++ webapp/src/data/brands.ts
@@ -0,0 +1,251 @@
+export interface BrandData {
+  name: string;
+  models: string[];
+}
+
+export const BRANDS: BrandData[] = [
+  {
+    name: "Alfa Romeo",
+    models: ["147", "156", "159", "166", "Giulia", "Giulietta", "MiTo", "Stelvio"],
+  },
+  {
+    name: "Audi",
+    models: ["A3", "A4", "A5", "A6", "A7", "A8", "e-tron", "Q3", "Q5", "Q7", "Q8", "RS6", "S4", "S5", "TT"],
+  },
+  {
+    name: "BAIC",
+    models: ["BJ40", "BJ80", "U5 Plus", "X35", "X55", "X7"],
+  },
+  {
+    name: "BMW",
+    models: ["1 Series", "3 Series", "5 Series", "7 Series", "X1", "X3", "X4", "X5", "X6", "X7", "Z4"],
+  },
+  {
+    name: "BYD",
+    models: ["Han", "Seal", "Song L", "Song Plus", "Tang", "Yuan Plus"],
+  },
+  {
+    name: "Cadillac",
+    models: ["CTS", "Escalade", "SRX", "XT4", "XT5", "XT6"],
+  },
+  {
+    name: "Changan",
+    models: ["Alsvin", "CS35 Plus", "CS55 Plus", "CS75 Plus", "CS95", "Eado Plus", "Lamore", "Uni-K", "Uni-T", "Uni-V"],
+  },
+  {
+    name: "Chery",
+    models: ["Amulet", "Arrizo 8", "Bonus", "Fora", "Kimo", "QQ", "Tiggo 4", "Tiggo 4 Pro", "Tiggo 7 Pro", "Tiggo 7 Pro Max", "Tiggo 8", "Tiggo 8 Pro", "Tiggo 8 Pro Max", "Tiggo e", "Very"],
+  },
+  {
+    name: "Chevrolet",
+    models: ["Aveo", "Blazer", "Camaro", "Captiva", "Cobalt", "Cruze", "Epica", "Lacetti", "Lanos", "Malibu", "Niva", "Orlando", "Spark", "Tahoe", "TrailBlazer", "Tracker"],
+  },
+  {
+    name: "Citroën",
+    models: ["Berlingo", "C-Crosser", "C-Elysée", "C3", "C4", "C4 Sedan", "C5", "C5 Aircross", "DS4", "DS5", "Jumpy"],
+  },
+  {
+    name: "Daewoo",
+    models: ["Espero", "Gentra", "Matiz", "Nexia"],
+  },
+  {
+    name: "Daihatsu",
+    models: ["Cuore", "Move", "Rocky", "Sirion", "Terios", "YRV"],
+  },
+  {
+    name: "Dodge",
+    models: ["Caliber", "Caravan", "Challenger", "Charger", "Durango", "Journey", "Nitro", "RAM"],
+  },
+  {
+    name: "Dongfeng",
+    models: ["580", "AX7", "DF6", "H30 Cross", "S30", "T5 EVO", "Warrior 007"],
+  },
+  {
+    name: "Exeed",
+    models: ["LX", "RX", "TXL", "VX"],
+  },
+  {
+    name: "FAW",
+    models: ["Bestune T55", "Bestune T77", "Bestune T99", "Oley", "V2", "V5", "Vita"],
+  },
+  {
+    name: "Fiat",
+    models: ["500", "Albea", "Bravo", "Doblo", "Ducato", "Grande Punto", "Linea", "Panda", "Punto", "Sedici"],
+  },
+  {
+    name: "Ford",
+    models: ["EcoSport", "Explorer", "Fiesta", "Focus", "Fusion", "Galaxy", "Kuga", "Maverick", "Mondeo", "Mustang", "Ranger", "S-MAX", "Transit"],
+  },
+  {
+    name: "GAZ",
+    models: ["3102", "3110", "3111", "31105", "69", "Gazelle", "Gazelle Next", "Sobol"],
+  },
+  {
+    name: "Geely",
+    models: ["Atlas", "Atlas Pro", "CK", "Coolray", "Emgrand 7", "Emgrand X7", "MK", "Monjaro", "Otaka", "Tugella"],
+  },
+  {
+    name: "Genesis",
+    models: ["G70", "G80", "G90", "GV60", "GV70", "GV80"],
+  },
+  {
+    name: "Great Wall",
+    models: ["Deer", "Hover", "Hover H3", "Hover H5", "Peri", "Safe", "Wingle"],
+  },
+  {
+    name: "Haval",
+    models: ["Dargo", "F7", "F7x", "H2", "H3", "H5", "H6", "H9", "Jolion", "M6"],
+  },
+  {
+    name: "Honda",
+    models: ["Accord", "Civic", "CR-V", "Fit", "HR-V", "Integra", "Jazz", "Legend", "Odyssey", "Pilot", "Stream"],
+  },
+  {
+    name: "Hongqi",
+    models: ["E-HS9", "H5", "H9", "HS5", "HS7"],
+  },
+  {
+    name: "Hyundai",
+    models: ["Accent", "Creta", "Elantra", "Getz", "Grand Starex", "i30", "i40", "ix35", "Palisade", "Santa Fe", "Solaris", "Sonata", "Starex", "Staria", "Tucson"],
+  },
+  {
+    name: "Infiniti",
+    models: ["EX", "FX35", "FX37", "FX45", "G25", "G35", "G37", "M35", "Q50", "Q70", "QX50", "QX56", "QX60", "QX70", "QX80"],
+  },
+  {
+    name: "Jaecoo",
+    models: ["J7", "J8"],
+  },
+  {
+    name: "Jaguar",
+    models: ["E-Pace", "F-Pace", "F-Type", "S-Type", "X-Type", "XE", "XF", "XJ"],
+  },
+  {
+    name: "Jeep",
+    models: ["Cherokee", "Commander", "Compass", "Grand Cherokee", "Renegade", "Wrangler"],
+  },
+  {
+    name: "Jetour",
+    models: ["Dashing", "T2", "X70", "X70 Plus", "X90 Plus"],
+  },
+  {
+    name: "Kia",
+    models: ["Carnival", "Ceed", "Cerato", "K5", "K900", "Mohave", "Optima", "Picanto", "Pro Ceed", "Rio", "Seltos", "Sorento", "Soul", "Spectra", "Sportage", "Stinger"],
+  },
+  {
+    name: "Lada",
+    models: ["2104", "2105", "2106", "2107", "2109", "2110", "2111", "2112", "2114", "2115", "21099", "4x4", "Granta", "Kalina", "Largus", "Niva Legend", "Niva Travel", "Priora", "Vesta", "XRAY"],
+  },
+  {
+    name: "Land Rover",
+    models: ["Defender", "Discovery", "Discovery Sport", "Freelander", "Range Rover", "Range Rover Evoque", "Range Rover Sport", "Range Rover Velar"],
+  },
+  {
+    name: "Lexus",
+    models: ["CT", "ES", "GS", "GX", "IS", "LC", "LS", "LX", "NX", "RX", "UX"],
+  },
+  {
+    name: "Lifan",
+    models: ["Breez", "Cebrium", "Murman", "Smily", "Solano", "X50", "X60", "X70"],
+  },
+  {
+    name: "Lixiang",
+    models: ["L7", "L8", "L9"],
+  },
+  {
+    name: "Mazda",
+    models: ["3", "5", "6", "323", "626", "CX-3", "CX-30", "CX-5", "CX-7", "CX-9", "Demio", "MPV", "MX-5", "Premacy", "Tribute"],
+  },
+  {
+    name: "Mercedes-Benz",
+    models: ["A-Class", "B-Class", "C-Class", "CLA", "CLS", "E-Class", "G-Class", "GLA", "GLB", "GLC", "GLE", "GLS", "ML", "S-Class", "Sprinter", "V-Class", "Vito"],
+  },
+  {
+    name: "Mini",
+    models: ["Clubman", "Cooper", "Cooper S", "Countryman", "Hatch", "Paceman"],
+  },
+  {
+    name: "Mitsubishi",
+    models: ["ASX", "Carisma", "Eclipse Cross", "Galant", "L200", "Lancer", "Outlander", "Pajero", "Pajero Sport", "Space Star"],
+  },
+  {
+    name: "Moskvich",
+    models: ["3", "3e", "5", "6"],
+  },
+  {
+    name: "Nissan",
+    models: ["Almera", "Almera Classic", "Juke", "Kicks", "Maxima", "Murano", "Navara", "Note", "Pathfinder", "Patrol", "Primera", "Qashqai", "Teana", "Terrano", "Tiida", "X-Trail"],
+  },
+  {
+    name: "Omoda",
+    models: ["C5", "S5"],
+  },
+  {
+    name: "Opel",
+    models: ["Antara", "Astra", "Corsa", "Frontera", "Insignia", "Meriva", "Mokka", "Omega", "Vectra", "Zafira"],
+  },
+  {
+    name: "Peugeot",
+    models: ["206", "207", "301", "307", "308", "3008", "408", "4008", "508", "5008", "Expert", "Partner", "Traveller"],
+  },
+  {
+    name: "Porsche",
+    models: ["718 Cayman", "911", "Boxster", "Cayenne", "Macan", "Panamera", "Taycan"],
+  },
+  {
+    name: "Renault",
+    models: ["Arkana", "Captur", "Duster", "Fluence", "Kangoo", "Kaptur", "Koleos", "Laguna", "Logan", "Master", "Megane", "Sandero", "Scenic", "Symbol", "Trafic"],
+  },
+  {
+    name: "SEAT",
+    models: ["Alhambra", "Altea", "Arona", "Ateca", "Ibiza", "Leon", "Toledo"],
+  },
+  {
+    name: "Skoda",
+    models: ["Fabia", "Karoq", "Kodiaq", "Octavia", "Rapid", "Roomster", "Superb", "Yeti"],
+  },
+  {
+    name: "SsangYong",
+    models: ["Actyon", "Actyon Sports", "Korando", "Kyron", "Musso", "Rexton", "Stavic", "Tivoli"],
+  },
+  {
+    name: "Subaru",
+    models: ["BRZ", "Forester", "Impreza", "Legacy", "Levorg", "Outback", "Tribeca", "WRX", "XV"],
+  },
+  {
+    name: "Suzuki",
+    models: ["Alto", "Grand Vitara", "Jimny", "Liana", "SX4", "Swift", "Vitara"],
+  },
+  {
+    name: "Tank",
+    models: ["300", "500", "700"],
+  },
+  {
+    name: "Tesla",
+    models: ["Model 3", "Model S", "Model X", "Model Y"],
+  },
+  {
+    name: "Toyota",
+    models: ["Alphard", "Avensis", "C-HR", "Camry", "Corolla", "Fortuner", "Highlander", "Hilux", "Land Cruiser", "Land Cruiser Prado", "Mark II", "Prius", "RAV4", "Supra", "Venza", "Yaris"],
+  },
+  {
+    name: "UAZ",
+    models: ["3151", "3303", "3909", "452", "469", "Hunter", "Patriot", "Pickup", "Profi"],
+  },
+  {
+    name: "Volkswagen",
+    models: ["Amarok", "Beetle", "Bora", "Caddy", "Caravelle", "Golf", "ID.4", "Jetta", "Multivan", "Passat", "Polo", "Taos", "Teramont", "Tiguan", "Touareg", "Touran", "Transporter"],
+  },
+  {
+    name: "Volvo",
+    models: ["C30", "S40", "S60", "S80", "S90", "V40", "V60", "V90", "XC40", "XC60", "XC70", "XC90"],
+  },
+  {
+    name: "Voyah",
+    models: ["Courage", "Dream", "Free"],
+  },
+  {
+    name: "Zeekr",
+    models: ["001", "007", "009", "X"],
+  },
+];
